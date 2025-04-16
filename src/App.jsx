1c232e3d6028debb5faab98057533d8462b29b8a
Grykus762 <Darkwolf762@live.com>
2025-04-16 10:13:50 -0700

    Converted to Vite Project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h2>Nav area (if necessary) here</h2>
+     </div>
+
+     <div>
+      <div>
+        <h2>Main Charsheet Window Here</h2>
+      </div>
+      <div>
+        <h2>Sidebar here</h2>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default App
